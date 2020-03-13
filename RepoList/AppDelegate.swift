a590a99c6120b979387c2807d8a0be1eb74ded56
Marina Azevedo Ferreira Soares <marina.soares@BRSPOBITANM0263.local>
2020-03-13 16:48:39 -0300

    inital commit

---
+++ RepoList/AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  RepoList
+//
+//  Created by Marina Azevedo Ferreira Soares on 13/03/20.
+//  Copyright © 2020 Marina Azevedo Ferreira Soares. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window?.tintColor = UIColor(named: "main")
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
+    
+}
+
